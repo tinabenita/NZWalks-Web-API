40bb71914ee45fef03c2371f6e3a968fb6dac482
tinabenita <tina1717rego@gmail.com>
2025-03-11 22:20:36 +0530

    feat: delete region by id - DELETE

--- NZWalks.Api/Controllers/RegionsController.cs
+++ NZWalks.Api/Controllers/RegionsController.cs
@@ -123,5 +123,22 @@ namespace NZWalks.Api.Controllers
             return Ok(regionDto);
 
         }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if(regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Regions.Remove(regionDomainModel);
+            dbContext.SaveChanges();
+
+            return Ok();
+        }
     }
 }
