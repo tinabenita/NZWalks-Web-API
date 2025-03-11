db139eefa234be0a3f2c2b5c4a015c613a011e87
tinabenita <tina1717rego@gmail.com>
2025-03-11 20:27:30 +0530

    feat: Added RegionsController.cs with a GetAll method returning Region objects.

---
+++ NZWalks.Api/Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.Api.Models.Domain;
+
+namespace NZWalks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = new Guid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://th.bing.com/th/id/OIP.GrH5anEAE10XhX7Mzs9a2wHaE7?rs=1&pid=ImgDetMain"
+                },
+                new Region
+                {
+                    Id = new Guid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://th.bing.com/th/id/OIP.GrH5anEAE10XhX7Mzs9a2wHaE7?rs=1&pid=ImgDetMain"
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
