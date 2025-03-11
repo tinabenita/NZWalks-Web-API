6089d447c517869ef6022196bb984156c7661af5
tinabenita <tina1717rego@gmail.com>
2025-03-11 17:40:03 +0530

    task: Add StudentsController; remove WeatherForecast classes

---
+++ NZWalks.Api/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        // GET: api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Tom", "Jerry", "Mickey", "Minnie" };
+            return Ok(studentNames);
+        }
+    }
+}
